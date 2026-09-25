6e4b03c453f3dea189b59b0104f6203106a54568
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2026-09-25 12:30:01 +0200

    Maths vendredi W4

--- math/integration_lebesgue/snippets.js
+++ math/integration_lebesgue/snippets.js
@@ -1,5 +1,7 @@
 const local_macros = {
+  "\\L": "\\mathscr{L}",
   "\\salg": "\\left\\langle #1 \\right\\rangle",
   "\\Fetag": "\\Func_{\\text{etagée}}",
   "\\pp": "\\operatorname{\\mathrm{p.p.}}",
+  "\\xalg": "\\boxtimes",
 };
--- theorie_groupes/proprietes_generales/snippets.js
+++ theorie_groupes/proprietes_generales/snippets.js
@@ -1,4 +1,5 @@
 const local_macros = {
+  "\\M": "\\mathscr{M}", // Modules de continuité
   "\\rel": "\\mathcal{R}",
   "\\commu": "[#1, #2]",
   "\\subnormal": "\\vartriangleleft",
